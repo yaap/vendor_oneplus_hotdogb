b3ef7f8a4795688f3b797b84a3119a24baa546b3
John Galt <johngaltfirstrun@gmail.com>
2025-03-27 10:45:13 -0400

    commonize mdm interface

--- hotdogb-vendor.mk
+++ hotdogb-vendor.mk
@@ -212,9 +212,4 @@ PRODUCT_PACKAGES += \
     libarm_proxy_skel \
     libgf_ud_hal \
     libhvx_proxy_stub \
-    libmdmdetect_vendor \
-    libmdmimgload \
-    liboemnvbk_img_helper \
-    sensors.ssc \
-    ks \
-    mdm_helper
+    sensors.ssc
